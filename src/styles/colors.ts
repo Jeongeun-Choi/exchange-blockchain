cc6b29a348b46ff66f0d36508c9bf38e8de9ef26
Jeongeun-Choi <cje3080@gmail.com>
2023-07-24 13:44:49 +0900

    add: 색상 관리 > colors 파일 생성

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  shade000: "#FAFBFC",
+  shade200: "#E0E2E8",
+  shade600: "#546182",
+  shade700: "#404E71",
+  shade800: "#313C57",
+  shade900: "#2A3249",
+
+  primary100: "#5D28F2",
+  primaryFont: "#5D28F2",
+
+  error100: "#F7254B",
+};
