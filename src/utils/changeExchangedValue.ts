77afa41495db7ef49189887c5f49a48c13e0a87b
Jeongeun-Choi <cje3080@gmail.com>
2023-07-27 22:08:45 +0900

    fix: type 분리 및 변수명 수정, 주석 추가

--- src/utils/changeExchangedValue.ts
+++ src/utils/changeExchangedValue.ts
@@ -1,28 +1,10 @@
+import { Props } from "./types";
+
 const defaultCoinCount: { [key: string]: number } = {
   Ethereum: 1,
   BnB: 50,
   Solana: 100,
 };
-
-interface CoinName {
-  fromCoinName: string;
-  toCoinName: string;
-}
-
-interface CoinValue {
-  fromCoin: string;
-  toCoin: string;
-}
-
-interface ExchangedInfo {
-  value: string;
-  exchangedType: "to" | "from";
-}
-interface Props {
-  exchangedInfo: ExchangedInfo;
-  onChangeFn: (coin: CoinValue) => void;
-  coinInfo: CoinName;
-}
 
 // value에 . 붙여주는 함수
 const parseFixedNum = (float: string, value: string) => {
@@ -48,19 +30,21 @@ export const changeExchangedValue = ({
     return;
   }
 
-  const multiRatio =
-    defaultCoinCount[toCoinName] / defaultCoinCount[fromCoinName];
-  const divideRatio =
+  // from input 입력시 to input 영역에 fromCount에서 얼마의 비율을 곱해야 toCount가 나오는지 알기 위한 비율 계산
+  const toRatio = defaultCoinCount[toCoinName] / defaultCoinCount[fromCoinName];
+
+  // to input 입력시 from input 영역에 toCount에서 얼마의 비율을 곱해야 fromCount가 나오는지 알기 위한 비율 계산
+  const fromRatio =
     defaultCoinCount[fromCoinName] / defaultCoinCount[toCoinName];
 
   coin.fromCoin =
     exchangedType === "from"
       ? value
-      : parseFixedNum(float, (parseFloat(value) * divideRatio).toString());
+      : parseFixedNum(float, (parseFloat(value) * fromRatio).toString());
   coin.toCoin =
     exchangedType === "to"
       ? value
-      : parseFixedNum(float, (parseFloat(value) * multiRatio).toString());
+      : parseFixedNum(float, (parseFloat(value) * toRatio).toString());
 
   onChangeFn(coin);
 };
